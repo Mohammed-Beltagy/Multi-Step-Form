8e2bd589db3e92712d38c346397f1d6070e4964a
Mohammed-Beltagy <justbeltagy@gmail.com>
2023-03-01 08:55:08 +0200

    Finished LG-Screens Design & Made The Footer Position Fixed For SM-Screens

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ function moveSlider() {
   slider.style.transform = `translateX(-${((step - 1) * 100) / stepsCount}%)`;
 }
 moveSlider();
+const next = document.getElementById("next"),
+  back = document.getElementById("back");
+next.onclick = function () {
+  if (step >= 4) {
+    return;
+  }
+  step++;
+  moveSlider();
+};
+back.onclick = function () {
+  if (step <= 1) {
+    return;
+  }
+  step--;
+  moveSlider();
+};
